563f9a9f67f969776abfad78a38c5dad4d230c18
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-11-25 10:27:21 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './Redux/store';
 import { store } from './Redux/store';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
@@ -8,7 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
